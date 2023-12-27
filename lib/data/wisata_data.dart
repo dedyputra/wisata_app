e62ea9935530af645aa8a204ee71a31edec7ecb5
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2023-12-27 20:27:17 +0700

    update, wisata, screen, yaml & banyak update

---
+++ lib/data/wisata_data.dart
@@ -0,0 +1,211 @@
+import 'package:wisata_app/models/wisata.dart';
+
+// Data Objek Wisata Kabupaten Sidoarjo Jatim
+
+var wisataList = [
+// wisata 1
+  Wisata(
+    name: 'Alas Prombon',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description:
+        'Alas Prambon memiliki sejumlah fasilitas yang cukup memadai meliputi permainan outbound , pertanian, peternakan, perikanan, kolam renang, kamar mandi, mushola, kolam lumpur, dll. Alas Prambon melayani outbound tingkat PG ,TK ,SD , SMP , SMA sd. DEWASA .',
+    built: 'Tahun 2013',
+    type: 'Wisata',
+    imageAsset: 'images/AlasPrambon.jpg',
+    imageUrls: [
+      'https://www.infowisataoutbound.com/image-upload/ICE-BREAKING-SD-BANGKALAN---Copy.jpg',
+      'https://www.infowisataoutbound.com/image-upload/ICE-BREAKINGG.JPG',
+      'https://www.infowisataoutbound.com/image-upload/flying-fox.jpg',
+      'https://storage.googleapis.com/seo-cms/assets/large_Berkemah_dan_berlatih_kepemimpinan_446ce1d521/large_Berkemah_dan_berlatih_kepemimpinan_446ce1d521.jpg',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 2
+  Wisata(
+    name: 'Alun Alun Sidoarjo',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description:
+        'Alun-Alun Sidoarjo mempunyai daya tariknya tersendiri dibanding tempat wisata lainnya sehingga membuatnya jadi destinasi favorit wisatawan.',
+    built: 'Tahun 1946',
+    type: 'Wisata',
+    imageAsset: 'images/AlunAlunSidoarjo.jpg',
+    imageUrls: [
+      'https://salsawisata.com/wp-content/uploads/2023/07/alun-alun-sidoarjo-alamat.jpg',
+      'https://channel9.id/wp-content/uploads/2021/01/WhatsApp-Image-2021-01-21-at-15.03.04.jpeg',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 3
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 4
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 5
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 6
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 7
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 8
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 9
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 10
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 11
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+
+// wisata 12
+  Wisata(
+    name: '',
+    location: 'Kabupaten Sidoarjo, Jawa Timur',
+    description: '',
+    built: 'Tahun',
+    type: 'Wisata',
+    imageAsset: 'images/',
+    imageUrls: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    isFavorite: false,
+  ),
+];
---
+++ lib/screens/screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_app/screens/screen.dart';
+
+// detail Screen App
+class Screen extends StatelessWidget {
+  const Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
