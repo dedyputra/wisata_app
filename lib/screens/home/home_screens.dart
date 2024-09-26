457388611b2c69c220349744edb679876a08c152
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2024-09-26 22:15:39 +0700

    update add image, home screens

--- lib/screens/home/home_screens.dart
+++ lib/screens/home/home_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_app/utilities/colors.dart';
 
 class HomeScreens extends StatefulWidget {
   const HomeScreens({super.key});
@@ -10,20 +11,59 @@ class HomeScreens extends StatefulWidget {
 class _HomeScreensState extends State<HomeScreens> {
   @override
   Widget build(BuildContext context) {
+    var kWhiteClr = KWhiteClr;
     return Scaffold(
       body: Stack(
         children: [
           // Menambahkan Image Homepage
           Image.asset(
-            "assets/images/background.jpg",
+            "assets/images/sda.jpg",
             height: double.infinity,
             width: double.infinity,
             fit: BoxFit.cover,
           ),
-          Column(
-            children: [
-              // Text("Selamat Datang", style: ,)
-            ],
+          SafeArea(
+            child: Center(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Sidoarjo East Java",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: KWhiteClr,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Selamat Datang Di Wisata Sidoarjo Jawa Timur",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: kWhiteClr,
+                        fontSize: 16,
+                        height: 1.6,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 55,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: KWhiteClr,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const KPrimaClr = Color(0xffd99156);
+const Color KPrimaClr = Color(0xffd99156);
+const Color KWhiteClr = Color.fromARGB(255, 255, 255, 255);
