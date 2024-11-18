eb931d33ebcabf6d551ecba3e76287394c7c0dbf
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2024-11-19 02:37:10 +0700

    update

--- lib/screens/Home/home_page.dart
+++ lib/screens/Home/home_page.dart
@@ -1,67 +1,172 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_app/utilities/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isDark = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-          child: Column(
-            children: [
-              // Halaman App Bar
-              Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage("assets/images/boy.png"),
-                  ),
-                  const SizedBox(
-                    width: 17,
-                  ),
-                  RichText(
-                    text: const TextSpan(
-                      text: "Hello Welcome, ",
-                      style: TextStyle(
-                          fontFamily: kFontFamily, color: kBlackColor),
-                      children: [
-                        TextSpan(
-                          text: "Dedy",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
+    // Mengatur tema aplikasi sesuai dengan mode tampilan
+    final ThemeData themeData = ThemeData(
+      useMaterial3: true,
+      brightness: isDark ? Brightness.dark : Brightness.light,
+    );
+
+    return MaterialApp(
+      theme: themeData,
+      home: Scaffold(
+        backgroundColor: isDark ? Colors.black : Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Column(
+              children: [
+                // Halaman App Bar
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage("assets/images/boy.png"),
+                    ),
+                    const SizedBox(
+                      width: 17,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "Hello Welcome, ",
+                        style: TextStyle(
+                          fontFamily: kFontFamily,
+                          color: isDark ? Colors.white : kBlackColor,
                         ),
+                        children: const [
+                          TextSpan(
+                            text: "Dedy",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // Teks judul dengan warna yang berubah sesuai mode tampilan
+                Text(
+                  "Explore Wisata Sidoarjo",
+                  style: TextStyle(
+                    fontFamily: kFontFamily,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 27,
+                    color: isDark ? Colors.white : Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // SearchBar untuk mencari wisata
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SearchAnchor(
+                    builder:
+                        (BuildContext context, SearchController controller) {
+                      return SearchBar(
+                        hintText: "Silahkan Cari Wisata",
+                        controller: controller,
+                        padding: const WidgetStatePropertyAll<EdgeInsets>(
+                          EdgeInsets.symmetric(horizontal: 16.0),
+                        ),
+                        onTap: () {
+                          controller.openView();
+                        },
+                        onChanged: (_) {
+                          controller.openView();
+                        },
+                        leading: const Icon(Icons.search),
+                        trailing: <Widget>[
+                          Tooltip(
+                            message: 'Ubah Mode Tampilan',
+                            child: IconButton(
+                              isSelected: isDark,
+                              onPressed: () {
+                                setState(() {
+                                  isDark = !isDark;
+                                });
+                              },
+                              icon: const Icon(Icons.wb_sunny_outlined),
+                              selectedIcon:
+                                  const Icon(Icons.brightness_2_outlined),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                    suggestionsBuilder:
+                        (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'Wisata $index';
+                        return ListTile(
+                          title: Text(
+                            item,
+                            style: TextStyle(
+                              color: isDark ? Colors.white : Colors.black,
+                            ),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              controller.closeView(item);
+                            });
+                          },
+                        );
+                      });
+                    },
+                  ),
+                ),
+                // untuk tipe kategori wisata
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Category",
+                      style: TextStyle(
+                        fontFamily: kFontFamily,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Material(
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(100),
+                  child: Container(
+                    height: 70,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: kWhiteColor,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(),
                       ],
                     ),
                   ),
-                ],
-              ),
-              // Search untuk mencari wisata
-              const SizedBox(
-                height: 15,
-              ),
-              const Text(
-                "Explore Wisata Sidoarjo",
-                style: TextStyle(
-                    fontFamily: kFontFamily,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 27),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Silahkan Cari Wisata",
-                  suffixIcon: Icon(Icons.search),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -1,24 +1,27 @@
-// tinggal panggil variabelnya oke semangat :)
-
 import 'package:flutter/material.dart';
 
 const String kFontFamily = 'Poppins';
 
+// Gaya teks untuk heading
 const kHeadingStyle = TextStyle(
   fontFamily: kFontFamily,
   fontSize: 24,
   fontWeight: FontWeight.bold,
 );
 
+// Gaya teks untuk body
 const kBodyTextStyle = TextStyle(
   fontFamily: kFontFamily,
   fontSize: 16,
 );
 
+// Gaya teks untuk tombol
 const kButtonTextStyle = TextStyle(
   fontFamily: kFontFamily,
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
+// Warna
 const kBlackColor = Color(0xFF000000);
+const kWhiteColor = Color(0xFFFFFFFF); // Warna putih
